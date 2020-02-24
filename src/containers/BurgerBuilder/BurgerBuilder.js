480f090da3de725c4612ec57c3776da14ea402bd
Elias Habash <eliashabash23@yahoo.com>
2020-02-23 18:43:40 -0800

    Adding redux thunk and redux to handle asynchronous code

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import axios from '../../axios-orders';
 
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
-import axios from '../../axios-orders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-import * as actionTypes from '../../store/actions'
+import * as burgerBuilderActions from '../../store/actions/index';
 
 import Aux from "../../hoc/Aux";
 
@@ -16,19 +16,10 @@ class BurgerBuilder extends Component {
 
 	state = {
 		purchasing: false,
-		loading: false,
-		error: false
 	}
 
 	componentDidMount() {
 		console.log(this.props);
-		// axios.get('https://react-my-burger-80c9a.firebaseio.com/ingredients.json')
-		// 	.then(response => {
-		// 		this.setState({ ingredients: response.data });
-		// 	})
-		// 	.catch(error => {
-		// 		this.setState({ error: true });
-		// 	});
 	}
 
 	updatePurchaseState = (ingredients) => {
@@ -43,37 +34,6 @@ class BurgerBuilder extends Component {
 		return sum > 0;
 	}
 
-	// addIngredientHandler = (type) => {
-	// 	const oldCount = this.state.ingredients[type];
-	// 	const updatedCount = oldCount + 1;
-	// 	const updatedIngredients = {
-	// 		...this.state.ingredients
-	// 	}
-	// 	updatedIngredients[type] = updatedCount;
-	// 	const priceAddition = INGREDIENT_PRICES[type];
-	// 	const oldPrice = this.state.totalPrice;
-	// 	const newPrice = oldPrice + priceAddition;
-	// 	this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
-	// 	this.updatePurchaseState(updatedIngredients);
-	// }
-
-	// removeIngredientHandler = (type) => {
-	// 	const oldCount = this.state.ingredients[type];
-	// 	if (oldCount <= 0) {
-	// 		return;
-	// 	}
-	// 	const updatedCount = oldCount - 1;
-	// 	const updatedIngredients = {
-	// 		...this.state.ingredients
-	// 	}
-	// 	updatedIngredients[type] = updatedCount;
-	// 	const priceDeduction = INGREDIENT_PRICES[type];
-	// 	const oldPrice = this.state.totalPrice;
-	// 	const newPrice = oldPrice - priceDeduction;
-	// 	this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
-	// 	this.updatePurchaseState(updatedIngredients);
-	// }
-
 	purchaseHandler = () => {
 		this.setState({purchasing: true});
 	}
@@ -81,19 +41,6 @@ class BurgerBuilder extends Component {
 	purchaseCancelHandler = () => {
 		this.setState({purchasing: false});
 	}
-
-	// purchaseContinueHandeler = () => {
-	// 	const queryParams = [];
-	// 	for (let i in this.state.ingredients) {
-	// 		queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-	// 	}
-	// 	queryParams.push('price=' + this.state.totalPrice);
-	// 	const queryString = queryParams.join('&');
-	// 	this.props.history.push({
-	// 		pathname: '/checkout',
-	// 		search: '?' + queryString
-	// 	});
-	// }
 
 	purchaseContinueHandeler = () => {
 		this.props.history.push('/checkout');
@@ -127,11 +74,7 @@ class BurgerBuilder extends Component {
 				price={this.props.price}
 				purchaseCancelled={this.purchaseCancelHandler}
 				purchaseContinued={this.purchaseContinueHandeler}/>
-		}
-		if (this.state.loading) {
-			orderSummary = <Spinner />
-		}
-		
+		}	
 		return (
 			<Aux>
 				<Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
@@ -152,8 +95,8 @@ const mapToStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
 	return {
-		onIngredientAdded: (ingName) => dispatch({ type: actionTypes.ADD_INGREDIENT, ingredientName: ingName}),
-		onIngredientRemoved: (ingName) => dispatch({ type: actionTypes.REMOVE_INGREDIENT, ingredientName: ingName})
+		onIngredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
+		onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName))
 	}
 }
 
