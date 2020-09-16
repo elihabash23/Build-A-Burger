d5b6657f67898fad89c40013f05b2cb13b6b26e7
Elias Habash <eliashabash23@yahoo.com>
2020-09-16 02:39:22 -0700

    Updates

---
+++ precache-manifest.6be977ff247891b100e9e5d14c0650a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a6f53b7cc82c2d337598907b0853989",
+    "url": "/Build-A-Burger/index.html"
+  },
+  {
+    "revision": "aa5dc242b15aa2e55011",
+    "url": "/Build-A-Burger/static/css/main.38a025bf.chunk.css"
+  },
+  {
+    "revision": "885b1198684fdb187b86",
+    "url": "/Build-A-Burger/static/js/2.035b2b41.chunk.js"
+  },
+  {
+    "revision": "280b48dbc3281f342da16f66e41cf46c",
+    "url": "/Build-A-Burger/static/js/2.035b2b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa5dc242b15aa2e55011",
+    "url": "/Build-A-Burger/static/js/main.09fa96d4.chunk.js"
+  },
+  {
+    "revision": "5611fdf0a6fa868985da",
+    "url": "/Build-A-Burger/static/js/runtime-main.bbca33a6.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Build-A-Burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
